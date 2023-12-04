8faecf368b668fff02749154a9f26989ecef67b9
Simon Wicky <simon@nymtech.net>
2023-12-04 10:38:16 +0100

    day 1

---
+++ 1/1_1.py
@@ -0,0 +1,29 @@
+"""
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+Consider your entire calibration document. What is the sum of all of the calibration values?
+
+"""
+
+numbers = '0123456789'
+total = 0
+with open("input",'r') as f:
+	for line in f:
+		for c in line:
+			if c in numbers:
+				total += 10 * int(c)
+				break
+		for c in line[::-1]:
+			if c in numbers:
+				total += int(c)
+				break
+
+print(total)		
---
+++ 1/1_2.py
@@ -0,0 +1,58 @@
+"""
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+"""
+
+numbers = '0123456789'
+numbers_spelled = ['zero','one','two','three','four','five','six','seven','eight','nine']
+total = 0
+with open("input",'r') as f:
+	for line in f:
+		found = False
+		print(line)
+		for i in range(len(line)):
+			if not found and line[i] in numbers:
+				total += 10 * int(line[i])
+				
+				found = True
+			for v, n in enumerate(numbers_spelled):
+				try:
+					if not found and line[i:].index(n) == 0:
+						total += 10 * v
+						print(n)
+						found = True
+				except:
+					pass
+
+		found = False
+		for i in range(len(line)-1,-1,-1):
+			if not found and line[i] in numbers:
+				total += int(line[i])
+				print(line[i])
+				found = True
+			for v, n in enumerate(numbers_spelled):
+				try:
+					if not found and line[i:].index(n) == 0:
+						total += v
+						print(n)
+						found = True
+				except:
+					pass
+
+
+print(total)	
+
+
+
