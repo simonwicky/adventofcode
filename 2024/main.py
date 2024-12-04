70f546c72b1f8d035741ec9d2346648091035265
Simon Wicky <simon@nymtech.net>
2024-12-04 09:40:06 +0100

    dirty main

---
+++ 2024/main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+from contextlib import chdir
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument(
+    "day", type=int, help="Day to run"
+)
+
+argParser.add_argument(
+    "part", type=int, help="Part to run", choices=[1,2]
+)
+
+args = argParser.parse_args()
+
+if args.day < 1 or args.day > 25:
+    print("Invalid day")
+    exit(1)
+
+directory = f"{args.day}"
+filename = f"{args.day}_{args.part}.py"
+
+if  not os.path.exists(directory) or not os.path.exists(directory + "/" + filename):
+    print(f"Day {args.day} part {args.part} hasn't been done")
+    exit(1)
+
+with chdir(directory):
+    exec(open(filename).read())
