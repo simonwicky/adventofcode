8eee2ddf60b8b4b60c0f67271b3aac8ce17ef64a
Simon Wicky <simon@nymtech.net>
2025-12-09 14:38:25 +0100

    tweak

--- 2025/9/9_2.py
+++ 2025/9/9_2.py
@@ -6,38 +6,6 @@ def rec_size(tile_1 : Tile, tile_2: Tile) -> int:
     return (abs(tile_1[0] - tile_2[0]) + 1) * (abs(tile_1[1] - tile_2[1]) + 1)
 
 
-def is_green(tile : Tile, red_tiles: list[Tile]) -> bool:
-    # ray_casting : look at all horizontal edges on below of the tile
-    
-    nb_top = 0
-    nb_bottom = 0
-    for i in range(len(red_tiles)):
-        corner1, corner2 = red_tiles[i], red_tiles[i-1]
-        if corner1[0] == corner2[0]: # vertical edges
-            if corner1[1] > corner2[1]:
-               corner1, corner2 = corner2, corner1 # ensure corner 1 is first
-            if tile[0] == corner1[0] and tile[1] >= corner1[1] and tile[1] <= corner2[1]:
-                return True # on a vertical edge
-            
-        if corner1[1] == corner2[1]:
-            if corner1[0] > corner2[0]:
-               corner1, corner2 = corner2, corner1 # ensure corner 1 is higher
-
-            if tile[0] >= corner1[0] and tile[0] <= corner2[0]:
-
-                if tile[1] == corner1[1]:
-                    return True # on a horizontal edge
-                if tile[1] > corner2[1]:
-                    nb_top += 1 # below the edge
-                if tile[1] < corner1[1]: 
-                    nb_bottom += 1 # on top of the edge           
-
-    if nb_bottom == 0 or nb_top == 0:
-        return False
-    
-    return nb_bottom % 2 == 1 or nb_top % 2 == 1
-
-
 # I know this fails on some edge cases
 def crossing(line_start : Tile, line_end : Tile, red_tiles: list[Tile]) -> bool:
     for i in range(len(red_tiles)):
@@ -63,16 +31,13 @@ def crossing(line_start : Tile, line_end : Tile, red_tiles: list[Tile]) -> bool:
     return False
              
 
-## check that corners are in the shape and that edges are in the shape
+## check that edges are in the shape
+## input analysis showed that it's unneeded to check corners
 def green_perimeter(tile1: Tile, tile2: Tile, red_tiles: list[Tile]) -> bool:
     top_left = (min(tile1[0], tile2[0]),min(tile1[1], tile2[1])) 
     bot_right = (max(tile1[0], tile2[0]),max(tile1[1], tile2[1]))
     top_right = (top_left[0], bot_right[1])
     bot_left = (bot_right[0], top_left[1]) 
-
-    # if a corner isn't in the shape, reject it
-    if not is_green(top_left,red_tiles) or not is_green(top_right, red_tiles) or not is_green(bot_left, red_tiles) or not is_green(bot_right,red_tiles):
-        return False
 
     if (crossing(top_right, top_left, red_tiles)
         or crossing(top_right, bot_right, red_tiles)
