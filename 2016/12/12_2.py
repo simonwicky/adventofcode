90b6493d85482ce055679334de3f7941c8fe8917
Simon Wicky <simon@nymtech.net>
2025-12-12 17:10:34 +0100

    2016 day 12

---
+++ 2016/12/12_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+with open('input','r') as f:
+    data = f.read().splitlines()
+
+
+registers = {}
+registers["a"] = 0
+registers["b"] = 0
+registers["c"] = 1
+registers["d"] = 0
+
+
+idx = 0
+while idx >= 0 and idx < len(data):
+    instruction = data[idx].split()
+
+    if instruction[0] == "inc":
+        registers[instruction[1]] += 1
+    elif instruction[0] == "dec":
+         registers[instruction[1]] -= 1
+    elif instruction[0] == "cpy":
+        if instruction[1].isalpha():
+            registers[instruction[2]] = registers[instruction[1]]
+        else:
+            registers[instruction[2]] = int(instruction[1])
+    elif instruction[0] == "jnz":
+        if instruction[1].isalpha():
+            if registers[instruction[1]] != 0:
+                idx += int(instruction[2]) - 1 # -1 for the increment that happens regardless
+        else:
+            if int(instruction[1]) != 0:
+                idx += int(instruction[2]) - 1 # -1 for the increment that happens regardless
+
+    idx += 1
+
+print(registers["a"])
